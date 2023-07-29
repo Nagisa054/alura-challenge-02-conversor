4bc479b1e6fdb7a0a77aea3298373a2cfca6135a
Juan <ciclajp@gmail.com>
2023-07-28 19:54:24 -0500

    Test exitoso, la convercion funciona, pero muestra mensajes de error cuando los valores son los correctos ytambien cuando no lo son.

--- src/Test/TestFrame.java
+++ src/Test/TestFrame.java
@@ -4,7 +4,8 @@ import UI.Window;
 
 public class TestFrame {
     public static void main(String[] args) {
-         Window frame = new Window();
+        Window frame = new Window();
+        System.out.println("programa iniciado");
     }
 
 }
