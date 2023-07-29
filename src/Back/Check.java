87f4ed18e2c7119b355ac790034f724f2d5a1bee
Juan <ciclajp@gmail.com>
2023-07-28 21:04:41 -0500

    Error de validacion CheckBox y input resuelto.

--- src/Back/Check.java
+++ src/Back/Check.java
@@ -11,33 +11,29 @@ import java.util.regex.Pattern;
 public class Check extends Window{
 
    public static Boolean ValidarInput(String input){
-        Pattern patterm = Pattern.compile("[^.\\d]+", Pattern.CASE_INSENSITIVE);
+
+        Pattern patterm = Pattern.compile("[^\\d.]+", Pattern.CASE_INSENSITIVE);
         Matcher matcher = patterm.matcher(input);
-        if (!matcher.find()){
+
+        if (matcher.find()){
             WindowErrorInput errorInputFrame = new WindowErrorInput();
         }
+
         return !matcher.find();
-
     }
-
     public static Boolean ValidarCBox(int list1, int list2){
 
         if (list1 == 0 || list2 == 0) {
             WindowErrorCBox errorCBoxFrame = new WindowErrorCBox();
-
-
             System.out.println("error ChecBox valor 'list1, list2' ");
-
-
             return false;
         }
-
         if (list1 == list2) {
             WindowErrorCBox errorCBoxFrame = new WindowErrorCBox();
             System.out.println("error ChecBox valor 'list1, list2' ");
             return false;
         }
         return true;
-    }
 
+    }
 }
