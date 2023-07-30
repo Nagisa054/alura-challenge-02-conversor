7d4e246791c149e13e3f2d302ac0934247cb432d
Juan <ciclajp@gmail.com>
2023-07-30 16:39:29 -0500

    Creando archivo Main.java. Todo funciona perfecto.

--- src/Back/Main.java
+++ src/Back/Main.java
@@ -1,36 +1,11 @@
 package Back;
 
 import UI.Window;
-import UI.WindowErrorCBox;
-
-import java.awt.event.ActionEvent;
-import java.math.BigDecimal;
 
 public class Main extends Logica{
-
     public static void main(String[] args) {
-        Window frame = new Window();
-/*        String input;
-        int monedaEntrada;
-        int monedaSalida;
-        BigDecimal valorSalida;
-        boolean monedaValida;
-        boolean inputValido;
-
-
-
-            input = frame.getInputValue();
-            monedaEntrada = frame.getList1Value();
-            monedaSalida = frame.getList2Value();
-
-            monedaValida = Check.ValidarCBox(monedaEntrada, monedaSalida);
-            inputValido = Check.ValidarInput(input);
-
-            */
-
-
-
-
+        System.out.println("iniciando...");
+        new Window();
+        System.out.println("programa iniciado");
     }
-
 }
