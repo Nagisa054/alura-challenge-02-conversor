335acdbb1e1b773a11aa47dbd4457ecbec97112c
Juan <ciclajp@gmail.com>
2023-07-29 22:00:02 -0500

    se inplementaron metodos de testing.  se encontró un error en la validacion del input

--- src/Test/TestCheck.java
+++ src/Test/TestCheck.java
@@ -2,27 +2,28 @@ package Test;
 
 import Back.Check;
 import UI.Window;
-import UI.WindowErrorCBox;
 
 public class TestCheck {
-    public static void main(String[] args) throws InterruptedException {
-        Window frame = new Window();
-        String input;
-        int valorList1;
-        int valorList2;
-        boolean inputValido;
-        boolean listValido;
+    private static Window frame;
+    private static void testCheckBoxes() throws InterruptedException {
         while (true) {
-            input = frame.getInputValue();
-            valorList1 = frame.getList1Value();
-            valorList2 = frame.getList2Value();
-            inputValido = Check.ValidarInput(input);
-            listValido = Check.ValidarCBox(valorList1, valorList2);
-            System.out.println(input + " es valido? " + inputValido);
-            System.out.println(valorList1 + " es valido? " + listValido);
-            System.out.println(valorList2 + " es valido? " + listValido);
-
-            Thread.sleep(5000);
+            Boolean cBox = Check.ValidarCBox(frame.getList1Value(), frame.getList2Value());
+            System.out.println("La Opcion: " + frame.getList1Value() + " y " + frame.getList2Value() + " es " + cBox);
+            Thread.sleep(2000);
         }
     }
+    private static void testInputvalue() throws InterruptedException{
+        while (true) {
+            Boolean input = Check.ValidarInput(frame.getInputValue());
+            System.out.println("El valor del input: " + frame.getInputValue() + " es " + input);
+            Thread.sleep(2000);
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        frame = new Window();
+//        testCheckBoxes();
+        testInputvalue();
+    }
+
 }
